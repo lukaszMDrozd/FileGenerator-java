9f1d09e9db6ad9ea1db9207d618c50423cd52573
Lukasz Drozd <principium17waw@gmail.com>
2019-07-01 18:35:15 +0200

    Pierwsza wersja aplikacji

---
+++ README.md
@@ -0,0 +1,6 @@
+# FileGenerator-java
+
+# Aplikacja służy do generowania plików xml w formacie pain.001.001.03;
+# do sterowania ilością plików służy zmienna "numberOfFiles";
+# do sterowania ilością transakcji w pliku służy zmienna "transactionNumberRange" - maksymalna liczba transakcji oraz "transactionNumberMinNumber" - minimalna liczba transakcji w pliku;
+# Wygenerowane pliku zostają umieszczone w katalogu /generatedFiles;
---
+++ generatorPlikow/src/main/java/com/lukasz/fileGenerator/App.java
@@ -0,0 +1,41 @@
+package com.lukasz.fileGenerator;
+
+import java.util.HashMap;
+
+public class App {
+    public static void main(String[] args) {
+
+        Long startTime;
+        Long stopTime;
+        Double resultTime;
+        int transactionNumberRange = 50;
+        int transactionNumberMinNumber = 50;
+        long transactionNumber;
+        long numberOfFiles = 1L;
+        int stringLength = 6;
+
+        startTime = System.currentTimeMillis();
+
+        for(int i = 0; i < numberOfFiles; i++){
+            if(transactionNumberMinNumber == transactionNumberRange){
+                transactionNumber = (long) (transactionNumberMinNumber);
+            } else {
+                transactionNumber = (long) (transactionNumberMinNumber + (transactionNumberRange - transactionNumberMinNumber) * Math.random());
+            }
+
+            double scale = Math.pow(10, 2);
+            double amount = Math.round(Math.random() * scale) / scale;
+
+            HashMap<Long, String> mapOfNumbers = GenerateMapOfIds.doGenerateMapOfIds(transactionNumber, stringLength);
+
+            CreateXMLFile.doCreateXMLFile(mapOfNumbers, amount);
+        }
+
+
+        stopTime = System.currentTimeMillis();
+        resultTime = ((stopTime - startTime)/1000.00);
+
+        System.out.println("Wygenerowano dane w czasie: " + String.format("%.2f", resultTime) + " sekund.");
+
+    }
+}
